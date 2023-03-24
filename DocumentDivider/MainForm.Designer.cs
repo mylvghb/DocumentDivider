42b105af071a1172f9d40a9194459bf44f8e5b75
daves <daves2009@126.com>
2023-03-24 13:31:36 +0800

    Initial commit

---
+++ DocumentDivider/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DocumentDivider
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDocPath = new System.Windows.Forms.Label();
+            this.lblSaveDir = new System.Windows.Forms.Label();
+            this.txtDocPath = new System.Windows.Forms.TextBox();
+            this.txtSaveDir = new System.Windows.Forms.TextBox();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDocPath
+            // 
+            this.lblDocPath.Location = new System.Drawing.Point(12, 26);
+            this.lblDocPath.Name = "lblDocPath";
+            this.lblDocPath.Size = new System.Drawing.Size(66, 18);
+            this.lblDocPath.TabIndex = 0;
+            this.lblDocPath.Text = "文档路径：";
+            // 
+            // lblSaveDir
+            // 
+            this.lblSaveDir.Location = new System.Drawing.Point(12, 59);
+            this.lblSaveDir.Name = "lblSaveDir";
+            this.lblSaveDir.Size = new System.Drawing.Size(66, 18);
+            this.lblSaveDir.TabIndex = 1;
+            this.lblSaveDir.Text = "保存目录：";
+            // 
+            // txtDocPath
+            // 
+            this.txtDocPath.Location = new System.Drawing.Point(75, 23);
+            this.txtDocPath.Name = "txtDocPath";
+            this.txtDocPath.Size = new System.Drawing.Size(281, 21);
+            this.txtDocPath.TabIndex = 2;
+            this.txtDocPath.Click += new System.EventHandler(this.txtDocPath_Click);
+            // 
+            // txtSaveDir
+            // 
+            this.txtSaveDir.Location = new System.Drawing.Point(75, 56);
+            this.txtSaveDir.Name = "txtSaveDir";
+            this.txtSaveDir.Size = new System.Drawing.Size(281, 21);
+            this.txtSaveDir.TabIndex = 3;
+            this.txtSaveDir.Click += new System.EventHandler(this.txtSaveDir_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(184, 106);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(75, 23);
+            this.btnSplit.TabIndex = 4;
+            this.btnSplit.Text = "分割";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(279, 106);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "退出";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Word 文档（*.doc,*.docx）|*.doc;*.docx";
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.Description = "请选择保存分割后的文件夹";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(75, 89);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(281, 17);
+            this.lblMessage.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 141);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.txtSaveDir);
+            this.Controls.Add(this.txtDocPath);
+            this.Controls.Add(this.lblSaveDir);
+            this.Controls.Add(this.lblDocPath);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文档分割器";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblMessage;
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+
+        private System.Windows.Forms.Label lblDocPath;
+        private System.Windows.Forms.Label lblSaveDir;
+        private System.Windows.Forms.TextBox txtDocPath;
+        private System.Windows.Forms.TextBox txtSaveDir;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Button btnQuit;
+
+        #endregion
+    }
+}
---
+++ DocumentDivider/MainForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Aspose.Words;
+
+namespace DocumentDivider
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDocPath_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            txtDocPath.Text = openFileDialog.FileName;
+            lblMessage.Text = string.Empty;
+        }
+
+        private void txtSaveDir_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            txtSaveDir.Text = folderBrowserDialog.SelectedPath;
+            lblMessage.Text = string.Empty;
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            var docPath = txtDocPath.Text.Trim();
+            if (string.IsNullOrEmpty(docPath))
+            {
+                MessageBox.Show("请选择要分割的 Word 文档", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var saveDir = txtSaveDir.Text.Trim();
+            if (string.IsNullOrEmpty(saveDir))
+            {
+                saveDir = Path.GetDirectoryName(docPath);
+            }
+
+            if (saveDir == null)
+            {
+                return;
+            }
+
+            try
+            {
+                ExtractPages(docPath, saveDir);
+                GC.Collect();
+                Process.Start(saveDir);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"分隔失败。\n错误信息：\t{ex.Message}\n堆栈跟踪：\t{ex.StackTrace}", "错误", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            btnQuit.Focus();
+        }
+
+        private static void ExtractPages(string docPath, string saveDir)
+        {
+            if (!Directory.Exists(saveDir))
+            {
+                Directory.CreateDirectory(saveDir);
+            }
+
+            var fileName = Path.GetFileName(docPath);
+
+            var original = new Document(docPath);
+            var fileFormat = original.OriginalLoadFormat switch
+            {
+                LoadFormat.Docx => SaveFormat.Docx,
+                LoadFormat.Rtf => SaveFormat.Rtf,
+                LoadFormat.Doc => SaveFormat.Doc,
+                _ => SaveFormat.Docx
+            };
+
+            //新建 Word 文档并向其中添加节
+            var index = 1;
+            //遍历原始文档的所有节
+            foreach (var secNode in original.Sections)
+            {
+                var sec = (Section) secNode;
+                var (newWord, newSection) = CreateDocumentWithSelection(sec);
+
+                //遍历每个节的所有 body 子对象
+                foreach (var secBodyChildNode in sec.Body.ChildNodes)
+                {
+                    // 如果对象是段落
+                    if (secBodyChildNode is Paragraph para)
+                    {
+                        //将原文档节中的段落对象添加到新文档的节中
+                        var newPara = newWord.ImportNode(para, true, ImportFormatMode.KeepSourceFormatting);
+                        newSection.Body.ChildNodes.Add(newPara);
+
+                        //遍历每个段落的所有子对象，并确定该对象是否是分页符
+                        foreach (var paraChildNode in para.ChildNodes)
+                        {
+                            var text = paraChildNode.GetText();
+                            //检测是否是分页符
+                            if (text == ControlChar.PageBreak)
+                            {
+                                //获取段落中分页符的索引
+                                var pageBreakIndex = para.ChildNodes.IndexOf(paraChildNode);
+
+                                //从段落中删除分页符
+                                newSection.Body.LastParagraph.ChildNodes.RemoveAt(pageBreakIndex);
+
+                                //保存新文档
+                                newWord.Save(Path.Combine(saveDir, $"P{index}-{fileName}"), fileFormat);
+                                index++;
+
+                                (newWord, newSection) = CreateDocumentWithSelection(sec); // 创建新文档并添加节
+
+                                //将原始节中的段落对象添加到新文档的节中
+                                newPara = newWord.ImportNode(para, true, ImportFormatMode.KeepSourceFormatting);
+                                newSection.Body.ChildNodes.Add(newPara);
+                                if (newSection.Body.FirstParagraph.ChildNodes.Count == 0)
+                                {
+                                    //删除第一个空白段落
+                                    newSection.Body.ChildNodes.RemoveAt(0);
+                                }
+                                else
+                                {
+                                    //删除分页符前的子对象
+                                    while (pageBreakIndex >= 0)
+                                    {
+                                        newSection.Body.FirstParagraph.ChildNodes.RemoveAt(pageBreakIndex);
+                                        pageBreakIndex--;
+                                    }
+                                    
+                                    // 如果还存在空白段落，删除该空白段落
+                                    if (newSection.Body.FirstParagraph.ChildNodes.Count == 0)
+                                    {
+                                        //删除第一个空白段落
+                                        newSection.Body.ChildNodes.RemoveAt(0);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // 将原文档节的其它对象添加到新文档的节中
+                        var newObj = newWord.ImportNode(secBodyChildNode, true, ImportFormatMode.KeepSourceFormatting);
+                        newSection.Body.ChildNodes.Add(newObj);
+                    }
+                }
+
+                // 保存到文件
+                newWord.Save(Path.Combine(saveDir, $"P{index}-{fileName}"), fileFormat);
+                index++;
+            }
+
+            // 创建一个文档并从另一个节中创建新节
+            (Document, Section) CreateDocumentWithSelection(Node section)
+            {
+                var newDoc = new Document();
+                newDoc.Sections[0].Remove(); // 移除默认节
+
+                var newSection = (Section) newDoc.ImportNode(
+                    section,
+                    true,
+                    ImportFormatMode.KeepSourceFormatting
+                );
+                newSection.HeadersFooters.Clear();
+                foreach (var headerFooterNode in ((Section) section).HeadersFooters)
+                {
+                    newSection.HeadersFooters.Add(newDoc.ImportNode(headerFooterNode, true,
+                        ImportFormatMode.KeepSourceFormatting));
+                }
+
+                newSection.ClearContent();
+                newSection.Body.FirstChild.Remove();
+
+                newDoc.Sections.Add(newSection);
+                return (newDoc, newDoc.FirstSection);
+            }
+        }
+    }
+}
